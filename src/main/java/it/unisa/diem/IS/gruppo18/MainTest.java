01712f2708f01171ce651a2c322b23153d67b771
igla22 <mariozitoscuola44@gmail.com>
2024-11-21 08:19:56 +0100

    Test repository

---
+++ src/main/java/it/unisa/diem/IS/gruppo18/MainTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.IS.gruppo18;
+
+/**
+ *
+ * @author gruppo18
+ */
+public class MainTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ciao, Mondo!");
+    }
+    
+}
